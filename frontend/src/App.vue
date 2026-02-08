840f0d925a24cf85feb144e9fa858fea677ae8d2
FilmkungW8 <manmanwer3344656@gmail.com>
2026-02-08 19:54:18 +0700

    feat: initialize quasar frontend structure

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          Midterm Exam
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <IndexPage />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script setup>
+import IndexPage from './pages/IndexPage.vue'
+</script>
---
+++ frontend/src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <h3>Welcome to Midterm Exam</h3>
+      <p>Data will appear here upon integration.</p>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
